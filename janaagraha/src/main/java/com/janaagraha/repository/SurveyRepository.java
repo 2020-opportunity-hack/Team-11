64df28346d7d704033b058bcf15432db10faef67
Prathika Gonchigar <pgonchigar@paypal.com>
2021-03-21 17:14:45 +0530

    removed the janaagraha folder

--- janaagraha/src/main/java/com/janaagraha/repository/SurveyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.janaagraha.repository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.janaagraha.entity.Survey;
-@Repository
-public interface SurveyRepository extends CrudRepository<Survey, Long> {
-
-}
